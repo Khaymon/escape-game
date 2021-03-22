7ab102a87d450dddcf48ee500d6ef8742cbb641e
Khaymon <khaymonenko@gmail.com>
2021-03-22 20:19:30 +0300

    Extra directories deleted

--- cmake-build-debug/CMakeFiles/room_escape.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for room_escape.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/khaymon/escape_game
-# Build directory: /home/khaymon/escape_game/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(UnitTests "bin/UnitTests")
-set_tests_properties(UnitTests PROPERTIES  _BACKTRACE_TRIPLES "/home/khaymon/escape_game/CMakeLists.txt;24;add_test;/home/khaymon/escape_game/CMakeLists.txt;0;")
